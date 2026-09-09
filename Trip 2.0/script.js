34816fc4f4311d0da9d7ef8fed4f5e493cbac833
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2026-09-09 10:19:11 -0400

    added event listener to submit button. user will now get a display on there screen as well as a console logged.

--- Trip 2.0/script.js
+++ Trip 2.0/script.js
@@ -3,4 +3,14 @@
 console.log("Hello!");
 
 const message = document.querySelector("#confirmation-message");
-console.log(message)
+//console.log(message)
+
+const form = document.querySelector("#trip-form");
+
+form.addEventListener("submit", function(event){
+
+    event.preventDefault();
+    message.textContent = "Get Ready!";
+    console.log("Your form was submitted! :)");
+
+});
