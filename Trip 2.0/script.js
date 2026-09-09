43d6b2d80759f2d8e468b67503b47c5b5d5242c2
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2026-09-09 19:55:10 -0400

    fixed the event listener to take the name input and display it on screen.

--- Trip 2.0/script.js
+++ Trip 2.0/script.js
@@ -2,15 +2,21 @@
 
 console.log("Hello!");
 
-const message = document.querySelector("#confirmation-message");
+
 //console.log(message)
 
 const form = document.querySelector("#trip-form");
 
+const nameInput= document.querySelector("#guest-name");
+const message = document.querySelector("#confirmation-message");
+
 form.addEventListener("submit", function(event){
 
     event.preventDefault();
-    message.textContent = "Get Ready!";
+    const name = nameInput.value;
+
+    message.textContent = "Get Ready!" + name + "New York here we come!";
     console.log("Your form was submitted! :)");
 
 });
+
