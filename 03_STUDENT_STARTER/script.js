db8f0192458646a1b72c9779b5517e0da7123fc4
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2026-09-21 21:30:55 -0400

    added files given in class today for next project titled student starter.

---
+++ 03_STUDENT_STARTER/script.js
@@ -0,0 +1,20 @@
+const page = document.querySelector("body");
+const vibeImage = document.querySelector("#vibe-image");
+const tagline = document.querySelector("#tagline");
+
+const luxuryButton = document.querySelector("#luxury-btn");
+const vacationButton = document.querySelector("#vacation-btn");
+const nightlifeButton = document.querySelector("#nightlife-btn");
+
+const budget = document.querySelector("#budget");
+const revealButton = document.querySelector("#reveal-btn");
+const result = document.querySelector("#result");
+
+/*
+DR. HOLLOMAN WILL ADD TWO FEATURES WITH YOU:
+1. Mood switcher: change the page color + image.
+2. Budget reveal: read a choice and display a result.
+
+THEN IT IS YOUR TURN:
+Add THREE noticeable JavaScript features of your own.
+*/
